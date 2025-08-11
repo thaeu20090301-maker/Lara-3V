8fa31aeeb3b1f8b006c680aaf6457942ba8f7df9
thaeu20090301-maker <thaeu20090301@gmail.com>
2025-08-11 23:32:18 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Your_Session_Id", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "WUxAxLLQ#nED5z7nUJ92ESvEdjcZiv4PpaPyO7CGjOS12OjRjpM8", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 MODE: process.env.MODE || "public",  //public | private | inbox | group
 OWNER_NUMBER: process.env.OWNER_NUMBER || "94779062397",
